e7472e7f9a8a023247f92fd487159c16309901d1
Praveen Kumar  Thakur <pravee1701@gmail.com>
2025-04-07 11:16:07 +0530

    db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import http from 'http';
 import dotenv from 'dotenv';
 import app from './app.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
 const server = http.createServer(app);
 
+connectDBB();
 
 const PORT = process.env.PORT || 8080;
 
