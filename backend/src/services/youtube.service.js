0ea9cb24c61b9c74bab7dac9500cfb335b7477eb
Praveen Kumar  Thakur <pravee1701@gmail.com>
2025-04-12 09:48:14 +0530

    updated fetch video from playlist

--- backend/src/services/youtube.service.js
+++ backend/src/services/youtube.service.js
@@ -1,23 +1,53 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 
 export const fetchVideosFromPlaylist = async (playlistId) => {
+    let allVideos = [];
+    let nextPageToken = null;
     try {
-        const response = await axios.get(`${process.env.YOUTUBE_API_BASE_URL}/playlistItems`, {
-            params: {
+        do {
+            const params = {
                 part: 'snippet',
-                maxResult: 50,
+                maxResults: 50, 
                 playlistId,
                 key: process.env.YOUTUBE_API_KEY,
-            },
-        });
+            };
 
-        return response.data.items.map((item) => ({
-            title: item.snippet.title,
-            videoId: item.snippet.resourceId.videoId,
-            url: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
-        }));
+            if (nextPageToken) {
+                params.pageToken = nextPageToken;
+            } 
+
+            const response = await axios.get(`${process.env.YOUTUBE_API_BASE_URL}/playlistItems`, {
+                params: params, 
+            });
+
+            const items = response.data?.items || [];
+
+            const videosFromPage = items.map((item) => {
+                if (item.snippet && item.snippet.resourceId && item.snippet.resourceId.videoId) {
+                    return {
+                        title: item.snippet.title,
+                        videoId: item.snippet.resourceId.videoId,
+                        url: `https://www.youtube.com/watch?v=${item.snippet.resourceId.videoId}`,
+                    };
+                }
+
+                return null; 
+            }).filter(video => video !== null); 
+
+            allVideos = allVideos.concat(videosFromPage);
+
+            
+            nextPageToken = response.data.nextPageToken;
+
+        } while (nextPageToken); 
+
+        return allVideos;
+
     } catch (error) {
-        console.error("Error fetching videos from YouTube", error.message);
-        throw new Error("Failed to fetch videos from YouTube");
+
+        const errorMessage = error.response?.data?.error?.message || error.message;
+        console.error(`Error fetching videos from YouTube playlist ${playlistId}: ${errorMessage}`, error.stack);
+
+        throw new Error(`Failed to fetch all videos from YouTube playlist ${playlistId}`);
     }
-}
+};
